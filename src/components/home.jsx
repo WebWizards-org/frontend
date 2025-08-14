3ccaea05c7333269fe04da9ba3e42e53f73ed168
abhishekbhatia01 <abhibhatia0410@gmail.com>
2025-08-14 17:35:59 +0530

    refactor Home component to include Navbar and enhance layout with background image and content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
-export default function home() {
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+import HERO2 from '../assets/HERO2.jpg';
+export default function Home() {
   return (
-    <div>
-      <p className='bg-amber-400'>Hello</p>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div
+        style={{
+          backgroundImage: `url(${HERO2})`,
+        }}
+        className="flex flex-col items-center justify-center mt-19 relative min-h-[600px] bg-cover bg-center bg-no-repeat md:items-start">
+          <div className='absolute inset-0 bg-black/0'></div>
+          <div className='w-full md:text-start text-center md:w-[35%] md:ml-20'>
+            <h1 className="relative z-10 text-white text-[28px] font-bold md:text-[50px] sm:text-[40px]">
+            LEARN ANYTIME, <span className='text-blue-300'>GROW ANYWHERE</span>
+            </h1>
+            <p className='text-white/50 relative x-10'>Join thousands of learner mastering new skills with our interactive learning platform. Start your journey today and unlock your potential.</p>
 
-export default Home
+            <button className='bg-gradient-to-r from-[#1B3C53] to-[#2A5470] text-white x-10 relative px-4 py-3 rounded-xl text-xl font-bold mt-10 hover:bg-blue-600 hover:shadow-xl hover:scale-105 transition-all ease-in-out duration-350'>Get Started</button>
+          </div>
+        
+
+      </div>
+    </>
+  );
+}
